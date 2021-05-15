f5b62c2ea3ed63dfc516de311579f84c56eb9b82
CemAdg <cem.akdag@hotmail.de>
2021-05-15 21:20:54 +0200

    auth0 configurated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,14 @@
  * ensure all variables on this page match your project
  */
 
+
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
     url: 'cemakdag-services.eu', // the auth0 domain prefix
-    audience: 'drinks', // the audience set for the auth0 app
-    clientId: 'eNT9jvBWNmEbadKnXBVP0OhaJjD1FcYa', // the client id generated for the auth0 app
+    audience: '************', // the audience set for the auth0 app
+    clientId: '************', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
